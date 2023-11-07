5e94e015773df90a58cf2371ff6cb65a5260ea19
Aryan N. Jain <142812153+AryanJ2004@users.noreply.github.com>
2023-11-07 10:12:13 +0530

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<utility>
+using namespace std;
+int main()
+{
+typedef map<string,int>mapType;
+mapType populationMap;
+populationMap.insert(pair<string,int>("China",1339));
+populationMap.insert(pair<string,int>("India",1187));
+populationMap.insert(mapType::value_type("US",310));
+populationMap.insert(mapType::value_type("Idonesia",234));
+populationMap.insert(make_pair("Pakistan",170));
+populationMap.insert(make_pair("Brasil",193));
+mapType::iterator iter=--populationMap.end();
+populationMap.erase(iter);
+cout<<"Size Of PopulationMap:"<<populationMap.size()<<'\n';
+for(iter=populationMap.begin();iter!=populationMap.end();++iter)
+{
+cout<<iter->first<<":"
+<<iter->second<<" Million\n";
+}
+string country("Indonesia");
+iter=populationMap.find(country);
+if(iter!=populationMap.end())
+cout<<country<<"s populations is"<<iter->second<<"million\n";
+else
+cout<<"Key Is Not In PopulationMap"<<'\n';
+populationMap.clear();
+}
